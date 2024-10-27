229ffc98597996a39f0c761ff43db26b76971000
vstocks2024 <vstocks2024@gmail.com>
2024-10-27 10:48:40 +0530

    removed animation routes

--- src/app/animations/getAnimationDataById/[animationId]/route.ts
+++ src/app/animations/getAnimationDataById/[animationId]/route.ts
@@ -1,29 +1,9 @@
-import { prisma } from "@/prismaClient";
+
 import { NextRequest ,NextResponse } from "next/server";
 
 
 export async function GET(request:NextRequest,{ params }: { params: { animationId: string } } ){
-    if(!request) return NextResponse.json({error:"404 no request found"})
-      try{
-        const animationId=params.animationId;
-        console.log(animationId);
-        let data;
-
-        await prisma.animations.findUnique({ where:{
-            id: animationId
-        },
-      select:{
-        animation_data:true
-      }}).then((dbresolve) => {
-        console.log(dbresolve);
-        data=dbresolve;
-      }).catch(async (dbreject) => {
-        console.log(dbreject);
-        data= dbreject;
-      })
-      return NextResponse.json(data);
-    }
-    catch(error){
-        console.log(error);
-    }
+    
+      return NextResponse.json({});
+   
 }
--- src/hooks/useTemplateFetch.ts
+++ src/hooks/useTemplateFetch.ts
@@ -4,30 +4,31 @@ import { useState } from "react"
 export const useTemplateFetch=()=>{
     const [loading,setLoading]=useState(false);
     const {template}=useTemplate();
-    const handleTemplate=async()=>{
-       setLoading(true);
-       console.log(template);
-       try{
-         const resp=await fetch(`http://localhost:3000/animations/getAnimationDataById/${template}`,{
-            method:"GET",
-            headers:{"Content-Type":"application/json"},
-         });
-         if(resp.ok){
-            const data=await resp.json();
-           if(data.error) throw new Error("No Such Template Found");
-           console.log(data);
+   //  const handleTemplate=async()=>{
+   //     setLoading(true);
+   //     console.log(template);
+   //     try{
+   //       const resp=await fetch(`http://localhost:3000/animations/getAnimationDataById/${template}`,{
+   //          method:"GET",
+   //          headers:{"Content-Type":"application/json"},
+   //       });
+   //       if(resp.ok){
+   //          const data=await resp.json();
+   //         if(data.error) throw new Error("No Such Template Found");
+   //         console.log(data);
 
-         }else{
-            console.log("No such template found");
-         }
-       }
-       catch(error){
-         console.log(error);
-       }
-       finally{
-        setLoading(false);
-       }
+   //       }else{
+   //          console.log("No such template found");
+   //       }
+   //     }
+   //     catch(error){
+   //       console.log(error);
+   //     }
+   //     finally{
+   //      setLoading(false);
+   //     }
        
-    }
-    return {loading,handleTemplate};
+   //  }
+   //  return {loading,handleTemplate};
+   return {loading};
 }
